12f480db093d4bd3cd31c7568b971eadea7bc9d4
Agnibhas Chattopadhyay <agnibhas.chattopadhyay@knoldus.com>
2022-07-08 13:28:27 +0530

    Basic Skeleton

---
+++ learning/src/test/java/com/knoldus/learning/SpringbootMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoldus.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
