8abef97bf230ca965e82d176c98fb43cd5f2434e
Vaibhavp241 <vaibhav.pandey@knoldus.com>
2022-07-14 02:39:34 +0530

    Added Controller, Service, Repo and updated application properties file

--- learning/src/main/java/com/knoldus/learning/SpringbootMavenApplication.java
+++ learning/src/main/java/com/knoldus/learning/SpringbootMavenApplication.java
@@ -2,12 +2,14 @@ package com.knoldus.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.knoldus.learning.repository")
 public class SpringbootMavenApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMavenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMavenApplication.class, args);
+    }
 
 }
--- learning/src/main/java/com/knoldus/learning/controller/AnswerController.java
+++ learning/src/main/java/com/knoldus/learning/controller/AnswerController.java
@@ -3,23 +3,23 @@ package com.knoldus.learning.controller;
 import com.knoldus.learning.entity.AnswerEntity;
 import com.knoldus.learning.service.AnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class AnswerController {
 
     @Autowired
     private AnswerService answerService;
 
     @GetMapping("/answers")
-    public String showAnswers(Model model) {
+    public List<AnswerEntity> showAnswers(Model model) {
         List<AnswerEntity> listAnswers = answerService.listAllAnswers();
         model.addAttribute("listAnswers", listAnswers);
 
-        return "answers";
+        return listAnswers;
     }
 }
