e050536de3aa5c6932fda939eba1a219c0699ab8
agnic <agnibhas.chattopadhyay@knoldus.com>
2022-07-13 11:02:01 +0530

    Learn-B6 * Updated LoginEntity

--- learning/src/main/java/com/knoldus/learning/Entity/LoginEntity.java
+++ learning/src/main/java/com/knoldus/learning/Entity/LoginEntity.java
@@ -1,29 +1,27 @@
 package com.knoldus.learning.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.persistence.Table;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
 public class LoginEntity {
     @Id
-    private String userName;
-    @NotNull
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
     private String password;
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @Column(nullable = false)
+    private String email;
 }
